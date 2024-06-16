371f7ec62474974288a317a5c0baae2083aed9a8
Mehebul02 <mehebul2123@gmail.com>
2024-06-17 00:38:27 +0600

    loading and error page added

---
+++ src/app/categories/error.jsx
@@ -0,0 +1,13 @@
+
+'use client'
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+            <h1>Category Error Page....</h1>
+        </div>
+    );
+};
+
+export default error;
